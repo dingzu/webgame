59cd10aacd222ceb71a41aea27b2cedee8fe544f
tari404 <2680383605@qq.com>
2021-09-21 23:42:48 +0800

    add git pges auto deployment feature

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/webgame/'
-      : '/',
-    baseUrl: process.env.NODE_ENV === 'production'
-      ? '/webgame/'
-      : '/'
-  }
+  publicPath: './'
+}
